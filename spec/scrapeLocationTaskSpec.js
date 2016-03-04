4eb988e36def127df4a0c28041ad3cf31101d483
TomChambrier <tomw08@gmail.com>
2016-03-04 22:09:30 +0000

    new spec for casper scrape script and renamed all specs

---
+++ spec/scrapeLocationTaskSpec.js
@@ -0,0 +1,20 @@
+describe("Player", function() {
+  // var player;
+  // var song;
+  spyOn(proc, 'exec')
+//run your code
+  proc.exec.mostRecentCall.args[1](true)
+
+  beforeEach(function() {
+    // player = new Player();
+    // song = new Song();
+  });
+
+  it("should be able to play a Song", function() {
+    //demonstrates use of custom matcher
+    expect(1).toEqual(1);
+  });
+
+});
+
+// just return input and test to see when
