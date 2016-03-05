5315e12d3c4477d451da8b079376837efdda0d45
TomChambrier <tomw08@gmail.com>
2016-03-05 16:34:35 +0000

    removed dead file link in spec runner

--- spec/scrapeTasksSpec.js
+++ spec/scrapeTasksSpec.js
@@ -1,13 +1,42 @@
-var scrapeTasks
+var scrapeTasks = require('/src/server/scraping/scrapeTasks.js');
 
-describe("scrape tasks", function() {
+describe("scrape location", function() {
   // var player;
   // var song;
   //spyOn(proc, 'exec')
 //run your code
  // proc.exec.mostRecentCall.args[1](true)
 
-  describe("filter out links that are not listings", function() {
+  describe("casperjs link scraping script", function() {
+      beforeEach(function() {
+          // define mock object to test
+      });
+
+
+      it("should return a defined object when run", function() {
+          //reg ex should match listing links
+          expect(scrapeTasks.scrapeLinks('dundee').toBeDefined();
+          //regex should not match other links
+          expect(1).toEqual(1);
+      });
+
+      it("t", function() {
+
+      });
+
+
+      //it("can reference both scopes as needed", function () {
+      //     expect(3).toEqual(bar);
+      // });
+
+  })
+
+
+});
+
+
+
+describe("filter out links that are not listings", function() {
     beforeEach(function() {
       // player = new Player();
      // song = new Song();
@@ -16,19 +45,19 @@ describe("scrape tasks", function() {
 
     it("regex should only match valid listing links", function() {
         //reg ex should match listing links
-        expect(1).toEqual(1);
+        expect(scrapeTasks.scrapeLinks()1).toEqual(1);
         //regex should not match other links
         expect(1).toEqual(1);
     });
 
-    it("", function() {
+    it("t", function() {
 
     });
 
 
-     it("can reference both scopes as needed", function () {
-            expect(3).toEqual(bar);
-     });
+     //it("can reference both scopes as needed", function () {
+       //     expect(3).toEqual(bar);
+    // });
 
   })
 
