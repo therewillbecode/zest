7f12ae9cf9a597638dc9fa3d3d90e9e204cc7c9a
TomChambrier <tomw08@gmail.com>
2016-03-07 10:50:20 +0000

    added comments

--- src/server/scraping/getLocationLinks.js
+++ src/server/scraping/getLocationLinks.js
@@ -16,13 +16,20 @@ var casper = require('casper').create({
     }
 });
 
-var searchLocation = casper.cli.get(0); // location for which to retrieve listing
-var linksDumpArr = [];  // stores the list of scraped room links
+// location for which to retrieve listing
+var searchLocation = casper.cli.get(0);
+// stores the list of scraped room links
+var linksDumpArr = [];
+// selector for input box on home page form to type in location
 var searchFormSelector = '#location';
+// selector for homepage search button
 var searchFormSubmitBtnSelector = 'button#submit_location>span';
+// selector for a image on page which is used to know when full page content has loaded as img is slowest to load
 var pageImageSelector = '#site-content > div > div.sidebar > div.search-results > div.outer-listings-container.row-space-2 > div > div > div:nth-child(6) > div > div.panel-image.listing-img > a.media-photo.media-cover > div > img';
+// selector for pagination button
 var nextPageBtnSelector ='#site-content > div > div.sidebar > div.search-results > div.results-footer > div.pagination-buttons-container.row-space-8 > div.pagination.pagination-responsive > ul > li.next.next_page > a > i';
-var pageNo = 1;   // keeps track of results page casper is on
+// keeps track of results page casper is on
+var pageNo = 1;
 
 
 function getPageLinkElements() {
@@ -80,7 +87,9 @@ function scrapeLinksAndPaginate() {
 }
 
 
-// Define steps for casperjs process
+/*
+    Define steps for casperjs process
+*/
 casper.start('http://www.airbnb.co.uk/');
 
 casper.waitForSelector(searchFormSelector, function enterSearchLocation() {
@@ -93,7 +102,9 @@ casper.then(function collectAndPaginate(){
     scrapeLinksAndPaginate();
 });
 
-//run steps above
+/*
+    run steps above
+*/
 casper.run(function onCompletion() {
     this.echo(linksDumpArr);
     casper.exit();
