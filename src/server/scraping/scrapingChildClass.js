52e6cfeb7a3241973f759d50432336eb62b10a28
TomChambrier <tomw08@gmail.com>
2016-03-09 11:56:05 +0000

    moved scraping child cladss to its own file

---
+++ src/server/scraping/scrapingChildClass.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Tom on 09/03/2016.
+ */
+var EventEmitter = require('events').EventEmitter;
+
+// class that defines events for spawned casper child process to enhance communication to parent
+// enhances logging
+class scrapeChildProc extends EventEmitter{
+// possible events that can be emitted by child process object
+
+
+}
+
+exports.classes = {scrapeChildProc};
+//var MyClass = function () { … }
+// MyClass.prototype.whatever = function() { … }
+
