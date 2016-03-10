fb35f8bfbe0f0c146cc2a9e772a89cf178d90dd0
TomChambrier <tomw08@gmail.com>
2016-03-10 15:50:35 +0000

    moved function to task runner that deals with promise

--- src/server/scraping/Tests/casperChildProcess_Test.js
+++ src/server/scraping/Tests/casperChildProcess_Test.js
@@ -2,55 +2,77 @@
  * Created by Tom on 08/03/2016.
  */
 
+var events = require('events');
+
 var mocha = require('mocha');
 var chai = require('chai');
-var events = require('events');
-var chaiAsPromised = require("chai-as-promised");
-
-chai.use(chaiAsPromised);
-var nock = require('nock');
 var expect = require('chai').expect;
+var assert = require('chai').assert;
+
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
+var nock = require('nock');
+
+//var mockSpawn = require('mock-spawn');
+//var mySpawn = mockSpawn();
+//require('child_process').spawn = mySpawn;
+
 var scrapeListing = require('./../scrapeLinks').task;
 
-var mockSpawn = require('mock-spawn');
-var mySpawn = mockSpawn();
-require('child_process').spawn = mySpawn;
 
-// override child_process.spawn
-// this is a simplistic example; you can use a library like `mockery` to
-// set a new instance for every test. See examples/complete/test.js
-
-mySpawn.setDefault(mySpawn.simple(1 /* exit code */, 'hello world' /* stdout */));
+// dependency injection - override child_process.spawn
+//mySpawn.setDefault(mySpawn.simple(0 /* exit code */, 'hello world' /* stdout */));
 
 describe('#scrapeLinkschild', function(){
 
     describe("error handling", function(){
+
         before(function() {
-            mySpawn.sequence.add(function (cb) {
+         //   mySpawn.sequence.add(function (cb) {
                 // test the error handling when error is emitted by spawned child
-                this.emit('error', new Error('spawn ENOENT'));
+
                 // setTimeout(function() { return cb(8); }, 10);
-            });
+           // });
+        });
+
+        it('data should equal hello world', function(){
+
+            var promised =  scrapeListing.scrapeChildPromise();
+
+            return assert.isFulfilled(promised, "optional message");
+
+
+        });
+
+
+        before(function() {
+      //      mySpawn.sequence.add(function (cb) {
+                // test the error handling when error is emitted by spawned child
+    //            this.emit('error', new Error('spawn ENOENT'));
+                // setTimeout(function() { return cb(8); }, 10);
+    //        });
         });
 
         it('error is passed to node when spawned child emits error', function(){
 
             scrapeListing.scrapeLinks('dundee', function(error, data) {
-                return expect(error).to.eventually.not.be.null;
+                console.log(error)
+                return expect(error).to.eventually.not.be.ok();
 
             });
 
         });
 
         before(function() {
-            mySpawn.sequence.add(function (cb) {
+    //        mySpawn.sequence.add(function (cb) {
                 // test the error handling when error is emitted by spawned child
 
                 // setTimeout(function() { return cb(8); }, 10);
-            });
+  //          });
         });
 
-        it('error is not passed to node when spawned child doesn\'t emit error', function(){
+        it('error is not passed to node when spawned child does not emit error', function(){
 
             scrapeListing.scrapeLinks('dundee', function(error, data) {
                 return expect(error).to.eventually.be.null;
@@ -59,24 +81,18 @@ describe('#scrapeLinkschild', function(){
 
         });
 
-        before(function() {
-            mySpawn.sequence.add({throws:new Error('spawn ENOENT')});
-        });
-
-        it('error passed to node when raised by spawned child', function(){
-            // test the error handling when error in raised by spawned child
-            return expect(scrapeListing.scrapeLinks).to.eventually.throw(Error);
-        });
 
 
         before(function() {
-            mySpawn.sequence.add({throws:new Error('spawn ENOENT')});
+            //mySpawn.sequence.add({throws:new Error('spawn ENOENT')});
         });
 
-        it('errors should be logged', function(){
+        it('promise should be rejected if child exit code is not 0', function(){
             // test the error handling when error in raised by spawned child
-            return expect(scrapeListing.scrapeLinks).to.eventually.throw(Error);
+            scrapeListing.scrapeLinks('dundee', function(error, data) {
+                return expect(scrapeListing.scrapeLinks).to.be.rejected;
 
+            });
         });
 
 
